6b4118b3f89131aae838ec2ac83187d871697612
nelson-caberto <nelson-caberto@users.noreply.github.com>
2022-03-06 20:14:51 -0600

    rebuilding from the ground up with half duplex in mind

---
+++ Nelson/messageRGBPDbit_second/messageRGBPDbit_second.ino
@@ -0,0 +1,64 @@
+#define PR_Pin 0
+#define LEDr_Pin 10
+#define LEDg_Pin 9
+#define LEDb_Pin  8
+#define PR_reads 40
+
+byte tx, rx;
+
+void setup() {
+  pinMode(PR_Pin, INPUT);
+  pinMode(LEDr_Pin, OUTPUT);
+  pinMode(LEDg_Pin, OUTPUT);
+  pinMode(LEDb_Pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+byte readBit() {
+  long prAVG = 0;
+  for (int i = 0; i<PR_reads; i++) prAVG += analogRead(PR_Pin);
+  prAVG /= PR_reads;
+  switch(prAVG) {
+    case 100 ... 275: return 0; //red min
+    case 590 ... 620: return 1; //blue max
+    case 690 ... 710: return 2; //green max
+    case 860 ... 880: return 3; //red max
+    default: return 255;
+  }
+}
+
+void ledOff() {
+  switch(tx) {
+    case 0: analogWrite(LEDr_Pin,0); break;
+    case 1: digitalWrite(LEDb_Pin,LOW); break;
+    case 2: digitalWrite(LEDg_Pin,LOW); break;
+    case 3: digitalWrite(LEDr_Pin,LOW); break;
+    default: break;
+  }
+}
+
+void ledOn() {
+  switch(tx) {
+    case 0: analogWrite(LEDr_Pin,1); break;
+    case 1: digitalWrite(LEDb_Pin,HIGH); break;
+    case 2: digitalWrite(LEDg_Pin,HIGH); break;
+    case 3: digitalWrite(LEDr_Pin,HIGH); break;
+    default: break;
+  }
+}
+
+void sendBit() {
+  ledOn();
+  delay(75);
+  ledOff();
+  delay(75);
+}
+
+void loop() {
+  rx = readBit();
+  if (rx != 255) Serial.println(rx);
+  if (Serial.available() > 0) {
+    tx = Serial.parseInt();
+    sendBit();
+  }
+}
