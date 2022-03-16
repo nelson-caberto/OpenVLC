33fdbc3332e2cfa0ab18b0fec5d1934d5024affe
nelson-caberto <nelson610@live.missouristate.edu>
2022-03-16 02:56:03 -0500

    cleanup

--- Nelson/rpi_web/txvr.py
+++ Nelson/rpi_web/txvr.py
@@ -1,3 +1,4 @@
+from multiprocessing.reduction import DupFd
 from queue import Queue
 from threading import Thread
 from time import sleep
@@ -21,38 +22,53 @@ GPIO.setup(RGBg,GPIO.OUT) #GREEN
 GPIO.setup(RGBb,GPIO.OUT) #BLUE
 
 class txvr:
-	def __init__(self):
-		self.msgs = []
-		self.q = Queue()
-		self.process_t = Thread(target=self.__process)
-		self.process_t.start()
+    def __init__(self):
+        self.rxP = -1
+        self.msgs = []
+        self.q = Queue()
+        self.process_t = Thread(target=self.__process)
+        self.process_t.start()
+
+    def trimMsgs(self):
+        if len(self.msgs)>5:
+            self.msgs.pop(0)
+
+    def add(self,data):
+        self.msgs.append(data)
+        self.trimMsgs()
+        self.q.put(data)
+        print('Adding Message: ',data)
+        print('New Queue Size: ',self.q.qsize())
+        
+    def __readBit():
+        value = AnalogIn(ads, ADS.PO).value
+        if 3400 < value and value < 3430: return 0
+        if 3560 < value and value < 3590: return 1
+        if 3610 < value and value < 3640: return 2
+        if 3940 < value and value < 3960: return 3
+        return -1
+
+    def __processRX(self):
+        self.rx = self.__readBit()
+        if self.rx != self.rxP:
+            sleep(25/1000)
+            self.rx = self.__reaBit()
+        if self.rx != self.rxP:
+            if self.rx != -1:
+                print('RX ',self.rx)
+            self.rxP = self.rx
 	
-	def trimMsgs(self):
-		if len(self.msgs)>5:
-			self.msgs.pop(0)
-	
-	def add(self,data):
-		self.msgs.append(data)
-		self.trimMsgs()
-		self.q.put(data)
-		print('Adding Message: ',data)
-		print('New Queue Size: ',self.q.qsize())
-	
-	def __processRX(self):
-        # chan = AnalogIn(ads, ADS.P0)
-		pass
-	
-	def __processTX(self):
+    def __processTX(self):
         # GPIO.output(RGBr,GPIO.HIGH) 
         # GPIO.output(RGBr,GPIO.LOW)
-		if not self.q.empty():
-			tx = self.q.get()
-			print('TX ',tx)
-			self.q.task_done()
-			print('Remaining Queue Size: ',self.q.qsize())
-	
-	def __process(self):
-		while True:
-			self.__processRX()
-			self.__processTX()
-			sleep(4)
+        if not self.q.empty():
+            tx = self.q.get()
+            print('TX ',tx)
+            self.q.task_done()
+            print('Remaining Queue Size: ',self.q.qsize())
+
+    def __process(self):
+        while True:
+            self.__processRX()
+            self.__processTX()
+            sleep(4)
