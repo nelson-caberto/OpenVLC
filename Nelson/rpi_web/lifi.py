8d89970f6e554509c0c0095a6f7604c48405d7d9
nelson-caberto <nelson610@live.missouristate.edu>
2022-03-16 02:12:23 -0500

    finished basic web interface

--- Nelson/rpi_web/lifi.py
+++ Nelson/rpi_web/lifi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from queue import Queue
 from threading import Thread
 from time import sleep
@@ -7,23 +7,31 @@ app = Flask(__name__)
 
 class txvr:
 	def __init__(self):
+		self.msgs = []
 		self.q = Queue()
 		self.process_t = Thread(target=self.__process)
 		self.process_t.start()
 	
+	def trimMsgs(self):
+		if len(self.msgs)>5:
+			self.msgs.pop(0)
+	
 	def add(self,data):
+		self.msgs.append(data)
+		self.trimMsgs()
 		self.q.put(data)
 		print('Adding Message: ',data)
-		print('New Queue Size',self.q.qsize())
+		print('New Queue Size: ',self.q.qsize())
 	
 	def __processRX(self):
-		print('RX')
+		pass
 	
 	def __processTX(self):
 		if not self.q.empty():
 			tx = self.q.get()
 			print('TX ',tx)
 			self.q.task_done()
+			print('Remaining Queue Size: ',self.q.qsize())
 	
 	def __process(self):
 		while True:
@@ -35,12 +43,16 @@ msgs = txvr()
 
 @app.route('/')
 def root():
-	return "Hello"
+	return render_template('index.html')
 	
 @app.route('/add/<msg>')
 def add(msg):
 	msgs.add(msg)
 	return ''
+
+@app.route('/msgs')
+def getMsgs():
+	return jsonify(msgs.msgs)
 	
 @app.route('/favicon.ico')
 def nothing():
